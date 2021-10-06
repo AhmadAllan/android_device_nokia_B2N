39726ae721123dd6c349a8237665380c003d308f
AhmadAllan <aaa200265@gmail.com>
2021-10-06 12:28:31 +0300

    edited lineage.mk and added device.mk

--- lineage.mk
+++ lineage.mk
@@ -13,9 +13,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 # Inherit some common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-TODO: add this file
-# Inherit from jasmine_sprout device
-$(call inherit-product, $(LOCAL_PATH)/device.mk)
+# Inherit from B2N device
+$(call inherit-product, device/nokia/B2N/device.mk)
 
 PRODUCT_BRAND := Nokia
 PRODUCT_DEVICE := B2N
@@ -28,7 +27,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 TARGET_VENDOR_PRODUCT_NAME := B2N
 
-TODO: update it
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="B2N_00WW 10 QKQ1.190828.002 00WW_4_150 release-keys"
